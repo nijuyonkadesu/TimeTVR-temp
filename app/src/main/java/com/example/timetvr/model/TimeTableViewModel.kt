35a6fe899a86dd8c86ec5526ebaef3449635b519
nijuyonkadesu <24ksriramv@gmail.com>
2022-10-02 13:52:59 +0530

    reduce redundancy

--- app/src/main/java/com/example/timetvr/model/TimeTableViewModel.kt
+++ app/src/main/java/com/example/timetvr/model/TimeTableViewModel.kt
@@ -44,127 +44,27 @@ class TimeTableViewModel: ViewModel() {
             return subjects[0]
         }
         else{
-            when (theDay) {
-                "Monday" -> { // 5 6 6 6 9 4 2
-                    when (hour) {
-                        7 -> {
-                            return subjects[timeTable["Monday"]!![0]]
-                        }
-                        8 -> {
-                            return subjects[timeTable["Monday"]!![1]]
-                        }
-                        9 -> {
-                            return subjects[timeTable["Monday"]!![2]]
-                        }
-                        10 -> {
-                            return subjects[timeTable["Monday"]!![3]]
-                        }
-                        11 -> {
-                            return subjects[timeTable["Monday"]!![4]]
-                        }
-                        12 -> {
-                            return subjects[timeTable["Monday"]!![5]]
-                        }
-                        13 -> {
-                            return subjects[timeTable["Monday"]!![6]]
-                        }
-                    }
+            when (hour) {
+                7 -> {
+                    return subjects[timeTable[theDay]!![0]]
                 }
-                "Tuesday" -> { // 5 2 3 1 4 3 8
-                    if (hour == 7) {
-                        return subjects[5]
-                    }
-                    else if (hour == 8) {
-                        return subjects[2]
-                        //return listOf("AD18511/12", "DL Laboratory/ IoT Laboratory", 1)
-                    }
-                    else if (hour == 9) {
-                        return subjects[3]
-                    }
-                    else if (hour == 10 ) {
-                        //return listOf("AD18511/12", "DL Laboratory/ IoT Laboratory", 1)
-                        return subjects[1]
-                    }
-                    else if (hour == 11) {
-                        //return listOf("", "SEMINAR - I", 1)
-                        return subjects[4]
-                    }
-                    else if (hour == 12) {
-                        //return listOf("AD18503", "Internet Of Things towards Data Science", 1)
-                        return subjects[3]
-                    }
-                    else if (hour == 13) {
-                        return subjects[8]
-                    }
+                8 -> {
+                    return subjects[timeTable[theDay]!![1]]
                 }
-                "Wednesday" -> { // 5 7 7 7 2 8 3
-                    if (hour == 7) {
-                        return subjects[5]
-                    }
-                    else if (hour == 8) {
-                        return subjects[7]
-                    }
-                    else if (hour == 9) {
-                        return subjects[7]
-                    }
-                    else if (hour == 10 ) {
-                        return subjects[7]
-                    }
-                    else if (hour == 11) {
-                        return subjects[2]
-                    }
-                    else if (hour == 12) {
-                        return subjects[8]
-                    }
-                    else if (hour == 13) {
-                        return subjects[3]
-                    }
+                9 -> {
+                    return subjects[timeTable[theDay]!![2]]
                 }
-                "Thursday" -> { // 2 6 6 6 8 3 1
-                    if (hour == 7) {
-                        return subjects[2]
-                    }
-                    else if (hour == 8) {
-                        return subjects[6]
-                    }
-                    else if (hour == 9) {
-                        return subjects[6]
-                    }
-                    else if (hour == 10 ) {
-                        return subjects[6]
-                    }
-                    else if (hour == 11) {
-                        return subjects[8]
-                    }
-                    else if (hour == 12) {
-                        return subjects[3]
-                    }
-                    else if (hour == 13) {
-                        return subjects[1]
-                    }
+                10 -> {
+                    return subjects[timeTable[theDay]!![3]]
                 }
-                "Friday" -> { // 1 4 3 4 4 2 1
-                    if (hour == 7) {
-                        return subjects[1]
-                    }
-                    else if (hour == 8) {
-                        return subjects[4]
-                    }
-                    else if (hour == 9) {
-                        return subjects[3]
-                    }
-                    else if (hour == 10 ) {
-                        return subjects[4]
-                    }
-                    else if (hour == 11) {
-                        return subjects[2]
-                    }
-                    else if (hour == 12) {
-                        return subjects[1]
-                    }
-                    else if (hour == 13) {
-                        return subjects[10]
-                    }
+                11 -> {
+                    return subjects[timeTable[theDay]!![4]]
+                }
+                12 -> {
+                    return subjects[timeTable[theDay]!![5]]
+                }
+                13 -> {
+                    return subjects[timeTable[theDay]!![6]]
                 }
             }
         }
